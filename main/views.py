7a79cf9c0b974d3407f0d97146bc215b462e9873
Alex Lapin <meccos160@yandex.ru>
2022-10-24 21:51:10 +0500

    added pdf

--- main/views.py
+++ main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return HttpResponse("<h4>about</h4>")
+def test(request):
+    # return HttpResponse("<h4>about</h4>")
+    resp = ""
+    pdffile = "PGIdata_2015-4.pdf"
+
+    resp = pdffile
+
+
+    return HttpResponse(resp)
