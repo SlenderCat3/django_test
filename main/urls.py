3eff429c05333f23141508280e6b8389b7d739e4
Alex Lapin <meccos160@yandex.ru>
2022-10-30 16:12:05 +0500

    image output_32

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('', views.index),
     path('test', views.test),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
